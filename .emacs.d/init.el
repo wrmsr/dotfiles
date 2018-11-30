5c26a34a39d890e5542d5d5e26db8db0fce281ad
wrmsr <timwilloney@gmail.com>
2018-11-30 10:47:07 -0800

    --

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+;; bundled:
+;;  darkburn-theme-20141205.226
+;;  window-number-20140124.302
+
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
@@ -17,23 +21,24 @@
 
 (require 'cl)
 
-(let
-  ((packages '(
-     auto-compile
-     browse-kill-ring
-     buffer-move
-     evil
-     magit
-     s
-     undo-tree
-   ))
-   (updated))
+(let (
+      (packages
+       '(
+	 auto-compile
+	 evil
+	 magit
+	 undo-tree
+	 buffer-move
+	 browse-kill-ring
+	 s
+	 )
+       )
+      (updated nil))
   (dolist (p packages)
-    (unless (package-installed-p p)
-      (print p)
-      (unless updated
-        (package-refresh-contents)
-        (setq updated t))
+    (if (not (package-installed-p p))
+	(unless updated
+	  (package-refresh-contents)
+	  (setq updated 't))
       (package-install p))))
 
 (require 'darkburn-theme)
@@ -111,11 +116,13 @@
 (require 'evil)
 (evil-mode)
 
+(require 'buffer-move)
+
 (when (boundp 'evil-mode)
   (defun evil-search-symbol-back ()
     (interactive)
     (evil-search-word nil nil t))
-  
+
   (defun evil-search-symbol-forward ()
     (interactive)
     (evil-search-word t nil t))
@@ -125,7 +132,62 @@
   (define-key evil-operator-state-map "\C-l" 'evil-normal-state)
   ;; (define-key evil-motion-state-map "\C-l" 'evil-esc)
 
+  (global-set-key (kbd "C-x g") 'whitespace-mode)
+  (global-set-key (kbd "C-x G") 'whitespace-cleanup)
+
+  (global-set-key (kbd "C-z") nil)
+
+  (global-set-key (kbd "C-x [") 'buf-move-either-horiz)
+  (global-set-key (kbd "C-x ]") 'buf-move-either-vert)
+  (global-set-key (kbd "C-x t") 'toggle-truncate-lines)
+  (global-set-key (kbd "C-x j") 'occur)
+  (global-set-key (kbd "C-x 8 s") "\\s-*")
+  (global-set-key (kbd "C-x C-0") 'kill-other-windows)
+
   (define-key evil-insert-state-map "\C-l" 'evil-normal-state)
+
+  (define-key evil-normal-state-map [(shift return)] 'evil-open-above-no-insert)
+  (define-key evil-normal-state-map [(return)] 'evil-open-below-no-insert)
+  ; (define-key evil-normal-state-map (kbd "C-w C-q") 'crosshairs)
+  (define-key evil-normal-state-map (kbd "[") 'evil-backward-paragraph)
+  (define-key evil-normal-state-map (kbd "]") 'evil-forward-paragraph)
+  (define-key evil-normal-state-map (kbd "g s") 'sort-lines)
+
+  (define-key evil-normal-state-map " " 'evil-next-line-10)
+  (define-key evil-motion-state-map " " 'evil-next-line-10)
+  (define-key evil-motion-state-map (kbd "DEL") 'evil-previous-line-10)
+  (define-key evil-normal-state-map (kbd "DEL") 'evil-previous-line-10)
+
+  (define-key evil-normal-state-map "_" 'evil-previous-line-first-non-blank)
+  (define-key evil-normal-state-map "+" 'evil-next-line-first-non-blank)
+  (define-key evil-normal-state-map "=" 'evil-scroll-page-down)
+  (define-key evil-normal-state-map "-" 'evil-scroll-page-up)
+  (define-key evil-normal-state-map "\\" 'evil-indent)
+
+  (defun evil-next-line-10 ()
+    (interactive)
+    (evil-next-line 10))
+
+  (defun evil-previous-line-10 ()
+    (interactive)
+    (evil-previous-line 10))
+
+  (defface evil-vi-state-id-face '((t (:background "#000000" :foreground "#4545FF"))) "" :group 'evil)
+  (defface evil-emacs-state-id-face '((t (:background "#000000" :foreground "#FF3333"))) "" :group 'evil)
+  (defface evil-insert-state-id-face '((t (:background "#000000" :foreground "#33FF33"))) "" :group 'evil)
+  (defface evil-visual-state-id-face '((t (:background "#000000" :foreground "#AD1EC9"))) "" :group 'evil)
+  (defface evil-replace-state-id-face '((t (:background "#000000" :foreground "#FFFF33"))) "" :group 'evil)
+  (defface evil-motion-state-id-face '((t (:background "#000000" :foreground "#FF9500"))) "" :group 'evil)
+  (defface evil-operator-state-id-face '((t (:background "#000000" :foreground "#FF9500"))) "" :group 'evil)
+
+  (setq evil-normal-state-tag (propertize " <N> " 'face 'evil-vi-state-id-face))
+  (setq evil-emacs-state-tag (propertize " <E> " 'face 'evil-emacs-state-id-face))
+  (setq evil-visual-state-tag (propertize " <V> " 'face 'evil-visual-state-id-face))
+  (setq evil-insert-state-tag (propertize " <I> " 'face 'evil-insert-state-id-face))
+  (setq evil-motion-state-tag (propertize " <M> " 'face 'evil-motion-state-id-face))
+  (setq evil-replace-state-tag (propertize " <R> " 'face 'evil-replace-state-id-face))
+  (setq evil-operator-state-tag (propertize " <O> " 'face 'evil-operator-state-id-face))
+
   )
 
 (window-number-mode)
@@ -148,24 +210,76 @@
   (interactive "P")
   (if (integerp number)
       (if (= 10 number)
-          (select-window (minibuffer-window))
-        (let ((window (nth (1- number) (window-number-list))))
-          (if (and window (or (not (window-minibuffer-p window))
-                              (minibuffer-window-active-p window)))
-              (select-window window)
-            (error "No such window."))))))
+	  (select-window (minibuffer-window))
+	(let ((window (nth (1- number) (window-number-list))))
+	  (if (and window (or (not (window-minibuffer-p window))
+			      (minibuffer-window-active-p window)))
+	      (select-window window)
+	    (error "No such window."))))))
 
 (setq minor-mode-alist
       (filter
        '(lambda (x)
-          (not (memq (car x) '(idle-highlight-mode
-                               hi-lock-mode
-                               auto-complete-mode
-                               yas/minor-mode
-                               window-number-meta-mode
-                               window-number-mode))))
+	  (not (memq (car x) '(idle-highlight-mode
+			       hi-lock-mode
+			       auto-complete-mode
+			       yas/minor-mode
+			       window-number-meta-mode
+			       window-number-mode))))
        minor-mode-alist))
 
 (add-to-list 'mode-line-position
-             '(:eval (window-number-string)) t 'eq)
+	     '(:eval (window-number-string)) t 'eq)
 
+(which-func-mode)
+
+(set-face-background 'which-func "#000000")
+(set-face-foreground 'which-func "#FFFFFF")
+
+(setq which-func-format
+      '((:propertize "[ " face which-func)
+	(:propertize which-func-current
+		     local-map ,which-func-keymap
+		     face which-func)
+	(:propertize " ] " face which-func)))
+
+(setq-default mode-line-format
+	      '("%e"
+		mode-line-mule-info ;;UUU: UU-(DOS)
+		mode-line-client
+		mode-line-modified
+		mode-line-remote ;; -
+		mode-line-frame-identification ;; -F1
+		" "
+		mode-line-buffer-identification
+		" "
+		mode-line-position
+		(which-func-mode ("" which-func-format))
+		" "
+		(:eval (let ((v vc-mode)) (when (> (length v) 0) (list (replace-regexp-in-string "^ +" "" v) " "))))
+		mode-line-modes
+		(global-mode-string ("" global-mode-string))
+		))
+
+(setq mode-line-position (list
+			  '(:eval (let ((c (point))
+					(s (- (point-max) (point-min)))
+					(lc (line-number-at-pos))
+					(ls (line-number-at-pos (point-max))))
+				    (concat
+				     "%l,%c,"
+				     (number-to-string c)
+				     " "
+				     (if (> ls 0)
+				       (number-to-string (/ (* 100 lc) ls))
+				       "-")
+				     "%% "
+				     (number-to-string s))))
+			  " "))
+
+(setq-default mode-line-modes (delete "--" mode-line-modes))
+(setq-default mode-line-modified '(:eval (let ((s (format-mode-line "%*%+")))
+  (cond ((equal s "--") "") ((equal s "**") "M") ((equal s "%%") "R") ((equal s "%*") "RM") (t s)))))
+;; (setq-default mode-line-remote '("%@"))
+(setq-default mode-line-buffer-identification (propertized-buffer-identification " %b "))
+(setq mode-line-frame-identification '(:eval (chomp (mode-line-frame-control))))
